ce2b3216d218871dd998aa3299b1ad0d35d04700
Benjamin Goering <ben@bengo.co>
2019-06-10 12:34:15 -0400

    GraphqlWsOverWebSocketOverHttpExpressMiddleware responds to graphql-ws stop messages by telling GRIP gateway to unsubscribe from relevant channel

--- src/subscriptions-transport-ws-over-http/GraphqlWsOverWebSocketOverHttpExpressMiddleware.ts
+++ src/subscriptions-transport-ws-over-http/GraphqlWsOverWebSocketOverHttpExpressMiddleware.ts
@@ -9,6 +9,9 @@ import GraphqlWebSocketOverHttpConnectionListener, {
   getSubscriptionOperationFieldName,
   IConnectionListener,
   IGraphqlWsStartMessage,
+  IGraphqlWsStopMessage,
+  isGraphqlWsStartMessage,
+  isGraphqlWsStopMessage,
 } from "./GraphqlWebSocketOverHttpConnectionListener";
 
 interface ISubscriptionStoringMessageHandlerOptions {
@@ -29,45 +32,75 @@ const SubscriptionStoringMessageHandler = (
   options: ISubscriptionStoringMessageHandlerOptions,
 ) => async (message: string) => {
   const graphqlWsEvent = JSON.parse(message);
-  if (!graphqlWsEvent) {
+  if (!isGraphqlWsStartMessage(graphqlWsEvent)) {
     return;
   }
   const operationId = graphqlWsEvent.id;
-  switch (graphqlWsEvent.type) {
-    case "stop":
-      assert(operationId, "graphql-ws GQL_STOP message must have id");
-      const subscriptionRowsForThisEvent = await filterTable(
-        options.subscriptionStorage,
-        sub => {
-          return (
-            sub.connectionId === options.connection.id &&
-            sub.operationId === graphqlWsEvent.id
-          );
-        },
-      );
-      await Promise.all(
-        subscriptionRowsForThisEvent.map(sub =>
-          options.subscriptionStorage.delete({ id: sub.id }),
-        ),
-      );
-      break;
-    case "start":
-      assert(operationId, "graphql-ws GQL_START message must have id");
-      const payload = graphqlWsEvent.payload;
-      const query = payload && payload.query;
-      assert(query, "graphql-ws GQL_START message must have query");
-      const subscriptionFieldName = getSubscriptionOperationFieldName(
-        graphqlWsEvent.payload,
-      );
-      await options.subscriptionStorage.insert({
-        connectionId: options.connection.id,
-        id: uuidv4(),
-        operationId,
-        startMessage: message,
-        subscriptionFieldName,
-      });
-      break;
+  assert(operationId, "graphql-ws GQL_START message must have id");
+  const payload = graphqlWsEvent.payload;
+  const query = payload && payload.query;
+  assert(query, "graphql-ws GQL_START message must have query");
+  const subscriptionFieldName = getSubscriptionOperationFieldName(
+    graphqlWsEvent.payload,
+  );
+  await options.subscriptionStorage.insert({
+    connectionId: options.connection.id,
+    id: uuidv4(),
+    operationId,
+    startMessage: message,
+    subscriptionFieldName,
+  });
+};
+
+/** WebSocket Message Handler that calls a callback on graphql-ws start message */
+const GraphqlWsStartMessageHandler = (
+  onMessage: (startMessage: IGraphqlWsStartMessage) => Promise<string | void>,
+) => async (message: string) => {
+  const graphqlWsEvent = JSON.parse(message);
+  if (!isGraphqlWsStartMessage(graphqlWsEvent)) {
+    return;
   }
+  return onMessage(graphqlWsEvent);
+};
+
+/** WebSocket Message Handler that calls a callback on graphql-ws stop message */
+const GraphqlWsStopMessageHandler = (
+  onMessage: (stopMessage: IGraphqlWsStopMessage) => Promise<string | void>,
+) => async (message: string) => {
+  const graphqlWsEvent = JSON.parse(message);
+  if (!isGraphqlWsStopMessage(graphqlWsEvent)) {
+    return;
+  }
+  return onMessage(graphqlWsEvent);
+};
+
+/**
+ * WebSocket message handler that will watch for graphql-ws GQL_STOP events that stop subscriptions,
+ * and remove corresponding subscription records from subscriptionStorage.
+ */
+const SubscriptionDeletingMessageHandler = (
+  options: ISubscriptionStoringMessageHandlerOptions,
+) => async (message: string) => {
+  const graphqlWsEvent = JSON.parse(message);
+  if (!isGraphqlWsStopMessage(graphqlWsEvent)) {
+    return;
+  }
+  const operationId = graphqlWsEvent.id;
+  assert(operationId, "graphql-ws GQL_STOP message must have id");
+  const subscriptionRowsForThisEvent = await filterTable(
+    options.subscriptionStorage,
+    sub => {
+      return (
+        sub.connectionId === options.connection.id &&
+        sub.operationId === graphqlWsEvent.id
+      );
+    },
+  );
+  await Promise.all(
+    subscriptionRowsForThisEvent.map(sub =>
+      options.subscriptionStorage.delete({ id: sub.id }),
+    ),
+  );
 };
 
 type IMessageListener = IConnectionListener["onMessage"];
@@ -107,25 +140,92 @@ export const GraphqlWsOverWebSocketOverHttpExpressMiddleware = (
       const graphqlWsConnectionListener = GraphqlWebSocketOverHttpConnectionListener(
         {
           connection,
-          getGripChannel: options.getGripChannel,
-          getMessageResponse: AcceptAllGraphqlSubscriptionsMessageHandler({
-            onStart: options.onSubscriptionStart,
-            onStop: options.onSubscriptionStop,
-          }),
+          async getGripChannel(graphqlWsMessage) {
+            const startMessage: IGraphqlWsStartMessage = isGraphqlWsStartMessage(
+              graphqlWsMessage,
+            )
+              ? graphqlWsMessage
+              : await (async () => {
+                  const stopMessage: IGraphqlWsStopMessage = graphqlWsMessage;
+                  const allStoredSubscriptions = await options.subscriptionStorage.scan();
+                  // Look up the graphql-ws start message corresponding to this stop message from the subscriptionStorage
+                  const storedSubscriptions = await filterTable(
+                    options.subscriptionStorage,
+                    s => {
+                      return (
+                        s.operationId === stopMessage.id &&
+                        s.connectionId === connection.id
+                      );
+                    },
+                  );
+                  const storedSubscription = await (async () => {
+                    switch (storedSubscriptions.length) {
+                      case 0:
+                        return;
+                      case 1:
+                        return storedSubscriptions[0];
+                      default:
+                        // @TODO getGripChannel should support returning an array of channels
+                        console.warn(
+                          `Found multiple stored subscriptions matching GQL_STOP. Expected only one. Will only use the first.`,
+                        );
+                        return storedSubscriptions[0];
+                    }
+                  })();
+                  if (storedSubscription) {
+                    const parsedStartMessage = JSON.parse(
+                      storedSubscription.startMessage,
+                    );
+                    if (!isGraphqlWsStartMessage(parsedStartMessage)) {
+                      throw new Error(
+                        `Failed to parse IGraphqlWsStartMessage from subscription.startMessage: ${storedSubscription}`,
+                      );
+                    }
+                    return parsedStartMessage;
+                  }
+                  throw new Error(
+                    `Failed to retrieve IGraphqlWsStartMessage for graphql-ws stop message: ${stopMessage}`,
+                  );
+                })();
+            const gripChannel = options.getGripChannel(startMessage);
+            return gripChannel;
+          },
+          getMessageResponse: AcceptAllGraphqlSubscriptionsMessageHandler(),
         },
       );
+      const { subscriptionStorage } = options;
       /**
        * We also want to keep track of all subscriptions in a table so we can look them up later when publishing.
        * So this message handler will watch for graphql-ws GQL_START mesages and store subscription info based on them
        */
-      const storeSubscriptionsOnMessage = SubscriptionStoringMessageHandler({
-        connection,
-        subscriptionStorage: options.subscriptionStorage,
-      });
-      // So the returned onMessage is going to be a composition of the above message handlers
+      const storeSubscriptionsMessageHandler = SubscriptionStoringMessageHandler(
+        { connection, subscriptionStorage },
+      );
+      /** And a handler that will delete stored subscriptions when there are Stopped */
+      const deleteSubscriptionsOnStopMessageHandler = SubscriptionDeletingMessageHandler(
+        { connection, subscriptionStorage },
+      );
+      /**
+       * Returned onMessage is going to be a composition of the above message handlers.
+       * Note that storeSubscriptions happens at the beginning, and deleteSubscriptionsOnStop happens at the end.
+       * This way any message handlers in the middle can count on the stored subscription being in storage.
+       */
       const onMessage = composeMessageHandlers([
-        storeSubscriptionsOnMessage,
+        storeSubscriptionsMessageHandler,
         graphqlWsConnectionListener.onMessage,
+        deleteSubscriptionsOnStopMessageHandler,
+        // We want this at the end so we can rely on onSubscriptionStop being called after subscriptionStorage has been updated
+        GraphqlWsStartMessageHandler(async () => {
+          if (options.onSubscriptionStart) {
+            options.onSubscriptionStart();
+          }
+        }),
+        // We want this at the end so we can rely on onSubscriptionStop being called after subscriptionStorage has been updated
+        GraphqlWsStopMessageHandler(async () => {
+          if (options.onSubscriptionStop) {
+            options.onSubscriptionStop();
+          }
+        }),
       ]);
       return {
         ...graphqlWsConnectionListener,
